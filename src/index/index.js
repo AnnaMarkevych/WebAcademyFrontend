c65f0393b1580fc77f9b5c705552f817af70376a
anna <anyaskarlet@gmail.com>
2018-04-14 15:27:35 +0300

    1

--- src/index/index.js
+++ src/index/index.js
@@ -1,1 +1,6 @@
 import './index.scss';
+let rootElement = document.querySelector(".parent");
+let btn = rootElement.querySelector(".parent_btn");
+let block = rootElement.querySelector(".parent_block");
+
+btn.onclick
