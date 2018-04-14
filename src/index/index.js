5736351ff6127d2ef380cf72069daaf1cd30064a
anna <anyaskarlet@gmail.com>
2018-04-14 12:34:56 +0300

    Created widget for trafficLight. Added 5 traffic lights. Changed styles for traffic lights.

--- src/index/index.js
+++ src/index/index.js
@@ -1,51 +1,62 @@
 import './index.scss';
-let lampRed = document.querySelector(".lampRed");
-let lampYellow = document.querySelector(".lampYellow");
-let lampGreen = document.querySelector(".lampGreen");
-let tumbler = document.querySelector(".tumbler");
 
-let flag = false;
+widgetTrafficLight("#firstTrafficLight");
+widgetTrafficLight("#secondTrafficLight");
+widgetTrafficLight("#thirdTrafficLight");
+widgetTrafficLight("#foursTrafficLight");
+widgetTrafficLight("#fivesTrafficLight");
 
-lampRed.onclick = lightRed;
-lampYellow.onclick = lightYellow;
-lampGreen.onclick = lightGreen;
-tumbler.onclick = tumblerOn;
 
-function lightRed() {
-    if (flag) {
-        lampRed.classList.toggle("lightRed");
-        lampYellow.classList.remove("lightYellow");
-        lampGreen.classList.remove("lightGreen");
+function widgetTrafficLight(selectorId) {
+    let rootElement = document.querySelector(selectorId);
+    let lampRed = rootElement.querySelector(".lampRed");
+    let lampYellow = rootElement.querySelector(".lampYellow");
+    let lampGreen = rootElement.querySelector(".lampGreen");
+    let tumbler = rootElement.querySelector(".tumbler");
+
+    let flag = false;
+
+    lampRed.onclick = lightRed;
+    lampYellow.onclick = lightYellow;
+    lampGreen.onclick = lightGreen;
+    tumbler.onclick = tumblerOn;
+
+    function lightRed() {
+        if (flag) {
+            lampRed.classList.toggle("lightRed");
+            lampYellow.classList.remove("lightYellow");
+            lampGreen.classList.remove("lightGreen");
+        }
     }
-}
 
-function lightYellow() {
-    if (flag) {
-        lampYellow.classList.toggle("lightYellow");
-        lampRed.classList.remove("lightRed");
-        lampGreen.classList.remove("lightGreen");
+    function lightYellow() {
+        if (flag) {
+            lampYellow.classList.toggle("lightYellow");
+            lampRed.classList.remove("lightRed");
+            lampGreen.classList.remove("lightGreen");
+        }
     }
-}
 
-function lightGreen() {
-    if (flag) {
-        lampGreen.classList.toggle("lightGreen");
-        lampRed.classList.remove("lightRed");
-        lampYellow.classList.remove("lightYellow");
+    function lightGreen() {
+        if (flag) {
+            lampGreen.classList.toggle("lightGreen");
+            lampRed.classList.remove("lightRed");
+            lampYellow.classList.remove("lightYellow");
+        }
     }
-}
 
-function tumblerOn() {
-    tumbler.classList.toggle("tumblerOn");
-    if (tumbler.classList.contains("tumblerOn")) {
-        flag = true;
-        tumbler.innerHTML = "on";
-    } else {
-        flag = false;
-        tumbler.innerHTML = "off";
-        lampRed.classList.remove("lightRed");
-        lampYellow.classList.remove("lightYellow");
-        lampGreen.classList.remove("lightGreen");
+    function tumblerOn() {
+        tumbler.classList.toggle("tumblerOn");
+        if (tumbler.classList.contains("tumblerOn")) {
+            flag = true;
+            tumbler.innerHTML = "on";
+        } else {
+            flag = false;
+            tumbler.innerHTML = "off";
+            lampRed.classList.remove("lightRed");
+            lampYellow.classList.remove("lightYellow");
+            lampGreen.classList.remove("lightGreen");
+        }
     }
 }
 
