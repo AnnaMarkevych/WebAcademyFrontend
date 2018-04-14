dbcc9ca8c1d4748718da54e74a0b4c421558c209
anna <anyaskarlet@gmail.com>
2018-04-14 12:38:53 +0300

    delete about

--- config/base.js
+++ config/base.js
@@ -10,7 +10,7 @@ const baseConf = (_path) => {
     const VENDORS_NAME = 'vendors';
     const entry = {
         index: ['babel-polyfill', './src/index/index.js'],
-        about: ['babel-polyfill', './src/about/about.js']
+        // about: ['babel-polyfill', './src/about/about.js']
     };
 
     const plugins = Object.keys(entry).reduce((acc, name) => {
