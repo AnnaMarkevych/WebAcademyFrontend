d0bf89ce6dd95413d6ae17330d5ddec5096a468b
anna <anyaskarlet@gmail.com>
2018-04-16 14:06:25 +0300

    final practic1

--- src/index/index.js
+++ src/index/index.js
@@ -1,15 +1,20 @@
 import './index.scss';
 widget("#parentFirst");
 widget("#parentSecond");
+widget("#parentThird");
+widget("#parentFours");
+widget("#parentFives");
+
 
 function widget(selectorId) {
     let rootElement = document.querySelector(selectorId);
     let btn = document.createElement("button");
     let block = document.createElement("div");
+
     btn.classList.add("parent_btn");
     btn.textContent = 'on/off';
     block.classList.add("parent_block");
-    block.textContent = "Text";
+    block.textContent = "Text bbbbbbb";
     rootElement.appendChild(btn);
     rootElement.appendChild(block);
     // let block = rootElement.querySelector(".parent_block");
