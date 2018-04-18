4acfb674b74f60c37d31c1ac03c052f3b0e8cba2
anna <anyaskarlet@gmail.com>
2018-04-18 18:29:55 +0300

    css

--- src/index/index.js
+++ src/index/index.js
@@ -1,24 +1,29 @@
 import './index.scss';
-dropdown();
-function dropdown() {
-    let rootElement = document.querySelector(".accordion");
-    let el = rootElement.querySelector(".accordion-btn-header");
-    // let description = rootElement.querySelector(".description");
-
-    let flag = el.classList.contains(".opened");
-
-    el.addEventListener("click", () => {
-        if (flag) {
-            close();
-        } else {
-            open();
-        }
-    });
-
-    function open() {
-        el.classList.add(".opened");
-    }
-    function close() {
-        el.classList.remove(".opened");
-    }
-}
+// dropdown();
+// function dropdown() {
+//     let rootElement = document.querySelector(".accordion");
+//     let el = rootElement.querySelector(".accordion-btn-header");
+//     // let description = rootElement.querySelector(".description");
+//
+//     let flag = el.classList.contains("opened");
+//
+//     function toggle () {
+//         el.addEventListener("click", () => {
+//             if (flag) {
+//                 close();
+//             } else {
+//                 open();
+//             }
+//         });
+//     }
+//     toggle();
+//
+//     function open() {
+//         flag = true;
+//         el.classList.add("opened");
+//     }
+//     function close() {
+//         flag = false;
+//         el.classList.remove("opened");
+//     }
+// }
