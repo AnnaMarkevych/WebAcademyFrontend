bd29d3b3f9f946480133116b4a070d1ca9e94ada
anna <anyaskarlet@gmail.com>
2018-04-19 16:44:25 +0300

    2

--- src/index/index.js
+++ src/index/index.js
@@ -1,6 +1,15 @@
 import './index.scss';
+let accordion = document.querySelector(".accordion");
+
 let button = document.createElement("button");
 button.classList.add("accordion-btn-header");
+accordion.appendChild(button);
+button.textContent = "Header";
+
+let discription = document.createElement("div");
+discription.classList.add("accordion-description");
+accordion.appendChild(discription);
+discription.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 
 
