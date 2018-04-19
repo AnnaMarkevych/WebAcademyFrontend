d1e42edaddd5f9f82d55531b50775b59386d683d
anna <anyaskarlet@gmail.com>
2018-04-19 10:52:27 +0300

    test2

--- src/index/index.js
+++ src/index/index.js
@@ -1,30 +1,36 @@
 import './index.scss';
-dropdown("#headerFirst");
-dropdown("#headerSecond");
+document.querySelectorAll(".accordion-btn-header").forEach((item) => {
+    dropdown(item);
+    console.log(item);
+});
 
-function dropdown(SelectorId) {
-    let rootElement = document.querySelector(SelectorId);
-    let description = rootElement.querySelector(".accordion-description");
 
-    let flag = description.classList.contains("opened");
+function dropdown(element) {
+    element.addEventListener("click", (event)=> {
+        event.target.classList.toggle("opened");
+    });
 
-    function toggle () {
-        el.addEventListener("click", () => {
-            if (flag) {
-                close();
-            } else {
-                open();
-            }
-        });
-    }
-    toggle();
-
-    function open() {
-        flag = true;
-        description.classList.add("opened");
-    }
-    function close() {
-        flag = false;
-        description.classList.remove("opened");
-    }
+    // let description = rootElement.querySelector(".accordion-description");
+    //
+    // let flag = description.classList.contains("opened");
+    //
+    // function toggle () {
+    //     el.addEventListener("click", () => {
+    //         if (flag) {
+    //             close();
+    //         } else {
+    //             open();
+    //         }
+    //     });
+    // }
+    // toggle();
+    //
+    // function open() {
+    //     flag = true;
+    //     description.classList.add("opened");
+    // }
+    // function close() {
+    //     flag = false;
+    //     description.classList.remove("opened");
+    // }
 }
