d4d6aa90b333e4ec0a91c280c545a7cdc42230bb
anna <anyaskarlet@gmail.com>
2018-04-19 19:49:08 +0300

    Dynamic accordion

--- src/index/index.js
+++ src/index/index.js
@@ -1,29 +1,61 @@
+/**
+ * @param {HTMLElement} el
+ * @param {Array} optionList
+ */
+
 import './index.scss';
 let accordion = document.querySelector(".accordion");
 
-let button = document.createElement("button");
-button.classList.add("accordion-btn-header");
-accordion.appendChild(button);
-button.textContent = "Header";
+/**
+ * @param {Array} optionList
+ */
+function dynamicDropdown(optionList) {
+  optionList.forEach((obj) => {
+      let button = document.createElement("button");
+      button.classList.add("accordion-btn-header");
+      accordion.appendChild(button);
+      button.textContent = obj.tittle;
 
-let discription = document.createElement("div");
-discription.classList.add("accordion-description");
-accordion.appendChild(discription);
-discription.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+      let discription = document.createElement("div");
+      discription.classList.add("accordion-description");
+      accordion.appendChild(discription);
+      discription.textContent = obj.description;
+  })
+}
 
 
+const accordionList = [
+     {
+         tittle: "1Header",
+         description: "1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     },
+     {
+         tittle: "2Header",
+         description: "2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     },
+     {
+         tittle: "3Header",
+         description: "3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     },
+     {
+         tittle: "4Header",
+         description: "4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     },
+     {
+         tittle: "5Header",
+         description: "5Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     },
+ ];
 
 
-
-
+dynamicDropdown(accordionList);
 document.querySelectorAll(".accordion-btn-header").forEach((item) => {
     dropdown(item);
-    console.log(item);
 });
 
 
 function dropdown(element) {
-    element.addEventListener("click", (event)=> {
+    element.addEventListener("click", (event) => {
         event.target.classList.toggle("opened");
     });
 }
