60614ce1d08162f13342ba3d349635dc6953a1ae
anna <anyaskarlet@gmail.com>
2018-04-19 16:33:36 +0300

    1

--- src/index/index.js
+++ src/index/index.js
@@ -1,4 +1,12 @@
 import './index.scss';
+let button = document.createElement("button");
+button.classList.add("accordion-btn-header");
+
+
+
+
+
+
 document.querySelectorAll(".accordion-btn-header").forEach((item) => {
     dropdown(item);
     console.log(item);
@@ -9,28 +17,4 @@ function dropdown(element) {
     element.addEventListener("click", (event)=> {
         event.target.classList.toggle("opened");
     });
-
-    // let description = rootElement.querySelector(".accordion-description");
-    //
-    // let flag = description.classList.contains("opened");
-    //
-    // function toggle () {
-    //     el.addEventListener("click", () => {
-    //         if (flag) {
-    //             close();
-    //         } else {
-    //             open();
-    //         }
-    //     });
-    // }
-    // toggle();
-    //
-    // function open() {
-    //     flag = true;
-    //     description.classList.add("opened");
-    // }
-    // function close() {
-    //     flag = false;
-    //     description.classList.remove("opened");
-    // }
 }
