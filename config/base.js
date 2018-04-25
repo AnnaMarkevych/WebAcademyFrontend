b0a8186e9119fec9d72aa3ed4315dab51560d77b
anna <anyaskarlet@gmail.com>
2018-04-25 16:26:47 +0300

    Added fonts

--- config/base.js
+++ config/base.js
@@ -11,6 +11,7 @@ const baseConf = (_path) => {
     const entry = {
         index: ['babel-polyfill', './src/index/index.js'],
         // about: ['babel-polyfill', './src/about/about.js']
+        // styles: ['./src/common/styles']
     };
 
     const plugins = Object.keys(entry).reduce((acc, name) => {
