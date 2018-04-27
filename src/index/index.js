002f594d0221bc56cf848f03ebbd6cd7f3c3013b
anna <anyaskarlet@gmail.com>
2018-04-27 18:00:01 +0300

    refactoring code of class opened

--- src/index/index.js
+++ src/index/index.js
@@ -1,5 +1,12 @@
 import './index.scss';
-function widget(selectorId) {
+widget();
+function widget() {
+    let btnBurger = document.querySelector(".menu-burger");
+    let menu = document.querySelector(".menu");
 
+    btnBurger.addEventListener("click",toggle());
 
+    function toggle() {
+        menu.classList.toggle("opened")
+    }
 }
