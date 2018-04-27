dbcc9ae4a46ec3563f35ee2733ba41c008cfc3c8
anna <anyaskarlet@gmail.com>
2018-04-27 23:31:25 +0300

    Added js code for menu

--- src/index/index.js
+++ src/index/index.js
@@ -4,7 +4,7 @@ function widget() {
     let btnBurger = document.querySelector(".menu-burger");
     let menu = document.querySelector(".menu");
 
-    btnBurger.addEventListener("click",toggle());
+    btnBurger.addEventListener("click",toggle);
 
     function toggle() {
         menu.classList.toggle("opened")
