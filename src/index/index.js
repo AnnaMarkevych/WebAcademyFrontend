026e26bc29bb6338860d317d9cab2fbf320d711e
anna <anyaskarlet@gmail.com>
2018-05-01 00:03:19 +0300

    1

--- src/index/index.js
+++ src/index/index.js
@@ -1,1 +1,22 @@
 import './index.scss';
+function addClassOpen(htmlElement) {
+    htmlElement.addEventListener("click", (event) => {
+        htmlElement.classList.toggle("open");
+
+        console.log("Click " + htmlElement);
+        event.stopPropagation();
+    });
+}
+const tests = document.querySelectorAll(".test");
+tests.forEach((item) => {
+    addClassOpen(item);
+});
+
+const body = document.querySelector("body");
+body.addEventListener("click", () => {
+    tests.forEach((item) => {
+        item.classList.remove("open");
+        console.log("Remove");
+    })
+});
+
