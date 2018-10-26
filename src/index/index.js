10620c16e4ea0be3c363a183118f6d7a86e979ad
anna <anyaskarlet@gmail.com>
2018-10-26 14:27:46 +0300

    taskList

--- src/index/index.js
+++ src/index/index.js
@@ -1,22 +1,88 @@
 import './index.scss';
-function addClassOpen(htmlElement) {
-    htmlElement.addEventListener("click", (event) => {
-        htmlElement.classList.toggle("open");
 
-        console.log("Click " + htmlElement);
-        event.stopPropagation();
-    });
+
+class TaskList {
+    constructor (rootElement) {
+        this.rootElement = rootElement;
+        this.render();
+        this.handleEvents();
+        this.fetchListData();
+    }
+
+    render () {
+        this.rootElement.classList.add("taskList");
+
+        const tittle = document.createElement("h1");
+        tittle.textContent = "Task List";
+
+        this.form = document.createElement("form");
+        this.input = document.createElement("input");
+        this.ul = document.createElement("ul");
+
+        this.form.appendChild(this.input);
+
+        this.rootElement.appendChild(tittle);
+        this.rootElement.appendChild(this.form);
+        this.rootElement.appendChild(this.ul);
+    }
+
+    renderList(taskList) {
+        taskList.forEach((task) => {
+            this.renderOne(task);
+        })
+    }
+
+    renderOne(task){
+        const  li = document.createElement('li');
+        li.textContent = task.title;
+        this.ul.appendChild(li);
+    }
+
+    handleEvents() {
+        this.form.addEventListener("submit", (eventObject) => {
+            eventObject.preventDefault();
+            this.addTask();
+        });
+    }
+
+    addTask() {
+        const reguestBody = {
+            title: this.input.value
+        };
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:4001/list', true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(reguestBody));
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    this.renderOne(JSON.parse(xhr.response));
+                } else {
+                    console.error(xhr.status, xhr.statusText);
+                }
+            }
+        };
+        // console.log(reguestBody);
+    }
+
+
+    fetchListData() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:4001/list', true);
+        xhr.send();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    this.renderList(JSON.parse(xhr.response));
+                } else {
+                    console.error(xhr.status, xhr.statusText);
+                }
+            }
+        };
+    }
+
 }
-const tests = document.querySelectorAll(".test");
-tests.forEach((item) => {
-    addClassOpen(item);
-});
 
-const body = document.querySelector("body");
-body.addEventListener("click", () => {
-    tests.forEach((item) => {
-        item.classList.remove("open");
-        console.log("Remove");
-    })
-});
+new TaskList(document.querySelector('#taskList'));
+
 
